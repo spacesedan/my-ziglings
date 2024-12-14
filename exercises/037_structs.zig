cba53232c74e0b2e74e07d4470ebbb85c2809581
Jose Duarte <jduarte0912@gmail.com>
2024-12-14 12:30:52 -0800

    liking this alot

--- exercises/037_structs.zig
+++ exercises/037_structs.zig
@@ -35,6 +35,7 @@ const Role = enum {
 const Character = struct {
     role: Role,
     gold: u32,
+    health: u8,
     experience: u32,
 };
 
@@ -44,6 +45,7 @@ pub fn main() void {
         .role = Role.wizard,
         .gold = 20,
         .experience = 10,
+        .health = 100,
     };
 
     // Glorp gains some gold.
